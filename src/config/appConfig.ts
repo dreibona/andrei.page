0a783058d7083944e3a03ef8409a5c79951801e0
Dreibona <59962148+dreibona@users.noreply.github.com>
2023-03-20 20:36:12 -0300

    add head and appConfig

---
+++ src/config/appConfig.ts
@@ -0,0 +1,13 @@
+export const head = {
+  url: 'https://andrei.page',
+  title: 'Andrei Bonamin - UI Designer',
+  description:
+    "Explore the work of Andrei Bonamin's UI portfolio, showcasing creative design solutions and articles",
+  author: 'Andrei Bonamin',
+  keywords:
+    'UI, UX, Digital Design, Product Design, Frontend, Motion, Portfolio, Articles, User Interface, User Experience',
+  image: '/cover.jpg',
+  locale: 'en',
+  type: 'website',
+  robot: true
+}
