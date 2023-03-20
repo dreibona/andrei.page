0fc8c3053cb17db9cef60bd0cc55069d2dfcb696
Dreibona <59962148+dreibona@users.noreply.github.com>
2023-03-20 20:44:40 -0300

    add navigation

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,3 +1,10 @@
+const x = new Date()
+const d = x.getDate() + 1 < 10 ? '0' + (x.getDate() + 1) : x.getDate() + 1
+const m = x.getMonth() + 1 < 10 ? '0' + (x.getMonth() + 1) : x.getMonth() + 1
+const year = x.getFullYear()
+const date = d + '/' + m + '/' + year
+export { date, year }
+
 export const head = {
   url: 'https://andrei.page',
   title: 'Andrei Bonamin - UI Designer',
@@ -11,3 +18,10 @@ export const head = {
   type: 'website',
   robot: true
 }
+
+export const links = [
+  { label: 'Work', href: '/work/', ariaLabel: 'Work Page' },
+  { label: 'Blog', href: '/blog/', ariaLabel: 'Blog Page' },
+  { label: 'About', href: '/about/', ariaLabel: 'About me Page' },
+  { label: 'Contact', href: '/contact/', ariaLabel: 'Contact me Page' }
+]
