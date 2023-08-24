c3b1386a41ff3522d4662117853e4bf8543fa3c6
dreibona <59962148+dreibona@users.noreply.github.com>
2023-08-24 18:14:49 +0200

    simplify date formatting logic

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,8 +1,7 @@
-const x = new Date()
-const d = x.getDate() + 1 < 10 ? '0' + (x.getDate() + 1) : x.getDate() + 1
-const m = x.getMonth() + 1 < 10 ? '0' + (x.getMonth() + 1) : x.getMonth() + 1
-const year = x.getFullYear()
-const date = d + '/' + m + '/' + year
+const d = new Date()
+const z = (n: number) => (n < 10 ? '0' : '') + n
+const year = d.getFullYear()
+const date = `${z(d.getDate())}/${z(d.getMonth() + 1)}/${year}`
 export { date, year }
 
 export const head = {
@@ -13,13 +12,13 @@ export const head = {
   author: 'Andrei Bonamin',
   keywords:
     'UI, UX, Digital Design, Product Design, Frontend, Motion, Portfolio, Articles, User Interface, User Experience',
-  image: '/cover.jpg',
+  image: '../placeholder.jpg',
   locale: 'en',
   type: 'website',
   robot: true
 }
 
-export const links = [
+export const navigation = [
   { label: 'Work', href: '/work/', ariaLabel: 'Work Page' },
   { label: 'Blog', href: '/blog/', ariaLabel: 'Blog Page' },
   { label: 'About', href: '/about/', ariaLabel: 'About me Page' },
