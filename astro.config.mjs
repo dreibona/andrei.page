0d214ba9feff8de66b2c5dcc1bc9e8cdb9b7c1fc
dreibona <59962148+dreibona@users.noreply.github.com>
2023-09-03 21:03:42 +0200

    update to astro 3.0 and fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://andrei.page',
-  experimental: {
-    assets: true
-  },
   integrations: [
     tailwind({
       applyBaseStyles: true
